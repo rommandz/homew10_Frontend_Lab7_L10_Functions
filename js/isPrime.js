d178bfe75564554679ea925cfe1834d4e5c47c95
rommandz <rmandzyuk94@gmail.com>
2017-07-18 15:28:43 +0300

    Update isPrime.js

--- js/isPrime.js
+++ js/isPrime.js
@@ -1,12 +1,16 @@
 function isPrime(number) {
-var result = true;
-    for (var i = 2; i < number; i++) {
-        if (number % i === 0) {
-            result = false;
-            break;
+    var result ;
+    if (number > 0) {
+    result = true;
+        for (var i = 2; i < number; i++) {
+            if (number % i === 0) {
+                result = false;
+                break;
+            }
         }
+    } else {
+        result = false ;
     }
-    return result;
+    return result ;
 }
-
 //console.log(isPrime(prompt("enter number", "")));
